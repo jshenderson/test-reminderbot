2b6e214cf9c6a67fa7a827df827af7e0643df58c
Joshua Henderson <hendejs@auburn.edu>
2017-09-01 10:09:28 -0500

    added plugins to restify

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var connector = new builder.ChatConnector({
 
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
-server.get('/', restify.serveStatic({
+server.get('/', restify.plugins.serveStatic({
     directory: __dirname,
     default: '/index.html'
 }));
